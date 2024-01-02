fd668a39109535df6ecad749973e8a96bebb6d8c
zengxuebin <1812806483@qq.com>
2024-01-02 23:54:58 +0800

    init

---
+++ src/main/java/com/secondhandcar/trade/system/SecondHandCarTradeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.secondhandcar.trade.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandCarTradeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandCarTradeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/secondhandcar/trade/system/SecondHandCarTradeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondhandcar.trade.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandCarTradeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
